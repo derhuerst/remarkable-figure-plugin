b00dcd0fb1091bbd1f5b2c197002fe9732835baa
Jannis R <mail@jannisr.de>
2016-11-15 20:49:13 +0100

    implementation :clap:

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 'use strict'
 
-// todo
+const {replaceEntities, escapeHtml} = require('remarkable/lib/common/utils');
+
+
+
+const rule = (imageRule) => (tokens, i, opt, env) => {
+	const img = imageRule(tokens, i, opt, env)
+	return '<figure>' + img + '<figcaption>'
+	+ (tokens[i].title ? escapeHtml(replaceEntities(tokens[i].title)) : '')
+	+ '</figcaption></figure>'
+}
+
+const plugin = (md) => {
+	md.renderer.rules.image = rule(md.renderer.rules.image)
+}
+
+module.exports = plugin
